24f8a19947de4a9d5659819d522b129064d2b1bf
Jordan Cobb <jordancobb92@gmail.com>
2025-12-20 20:32:41 -0400

    Harden firebase-admin init for Vercel build

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,11 +1,16 @@
 import admin from "firebase-admin";
 
-if (!admin.apps.length) {
+const hasCredentials =
+  process.env.FIREBASE_PROJECT_ID &&
+  process.env.FIREBASE_CLIENT_EMAIL &&
+  process.env.FIREBASE_PRIVATE_KEY;
+
+if (!admin.apps.length && hasCredentials) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     }),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
