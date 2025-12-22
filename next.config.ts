3d6e64a7406194cdd2636504bb6eb8fbb359855f
Jordan Cobb <jordancobb92@gmail.com>
2025-12-22 13:32:47 -0400

    Fix configs for Next.js 15, update approveVendor URL

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
         pathname: "/v0/b/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
       },
     ],
   },
