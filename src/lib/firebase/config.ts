bd12a40a9403d9120f727351e17ccbb9f32c6ee9
Jordan Cobb <jordancobb92@gmail.com>
2026-02-09 16:11:45 -0400

    add closed store logic and home page adjustments

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,7 +5,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";
-
+export { app }
 // ------------------------------------------
 // FIREBASE CONFIG
 // ------------------------------------------
