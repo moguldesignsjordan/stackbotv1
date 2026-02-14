ca7caa764d621a5350bc95588217fe21e69bffc8
Jordan Cobb <jordancobb92@gmail.com>
2026-02-13 21:37:25 -0400

    fix: add GoogleAuth plugin config to capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,23 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.stackbotglobal.app',
   appName: 'StackBot',
-
+  webDir: 'out',
   server: {
     url: 'https://stackbotglobal.com',
-    cleartext: false,
+    cleartext: true,
   },
 
   plugins: {
     FirebaseAuthentication: {
       skipNativeAuth: false,  
       providers: ['google.com', 'apple.com'],
+    },
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '443911086980-3kcmk2kenug5evq8fe9td6r603ctp3g2.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
     },
   },
 };
